8cbc21b8d73f532acd0c80305212e34d48697c3c
scepan_jankovic <scepan.jankovic@material-exchange.com>
2021-11-30 14:52:19 +0100

    Barcode logic created

--- src/app/features/invoice-create-edit/invoice-order-printed/invoice-order-printed.module.ts
+++ src/app/features/invoice-create-edit/invoice-order-printed/invoice-order-printed.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { InvoiceOrderPrintedComponent } from './invoice-order-printed.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { FramingDescriptionModule } from 'src/app/shared/components/framing-description/framing-description.module';
+import { NgxBarcodeModule } from 'ngx-barcode';
 
 @NgModule({
   declarations: [InvoiceOrderPrintedComponent],
-  imports: [CommonModule, TranslateModule, FramingDescriptionModule],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    FramingDescriptionModule,
+    NgxBarcodeModule,
+  ],
   exports: [InvoiceOrderPrintedComponent],
 })
 export class InvoiceOrderPrintedModule {}
--- src/app/features/invoice-create-edit/invoice-printed/invoice-printed.module.ts
+++ src/app/features/invoice-create-edit/invoice-printed/invoice-printed.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { InvoicePrintedComponent } from './invoice-printed.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { FramingDescriptionModule } from 'src/app/shared/components/framing-description/framing-description.module';
+import { NgxBarcodeModule } from 'ngx-barcode';
 
 @NgModule({
   declarations: [InvoicePrintedComponent],
-  imports: [CommonModule, TranslateModule, FramingDescriptionModule],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    FramingDescriptionModule,
+    NgxBarcodeModule,
+  ],
   exports: [InvoicePrintedComponent],
 })
 export class InvoicePrintedModule {}
